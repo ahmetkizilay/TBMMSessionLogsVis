c604db0bf0e5f87971e425e6fd8e14181dac411c
ahmet kizilay <ahmet.kizilay@gmail.com>
2013-07-07 02:17:42 +0300

    fixed turkish characters

--- js/interaction.js
+++ js/interaction.js
@@ -1,5 +1,5 @@
 $("select").change(function () {
-	$("#info h2").text("TBMM " + $("select option:selected").text() + " Oturum Sureleri");
+	$("#info h2").text("TBMM " + $("select option:selected").text() + " Oturum Süreleri");
 
 	d3.select("svg").remove();
 	d3.json("data/" + $("select").val() + ".json", processData);
